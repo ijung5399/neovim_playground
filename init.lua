63f6ab9b77010dfc50628c51e6f4bfc4804dd8fb
Ilmo Jung <ilmojung@microsoft.com>
2025-06-03 12:14:02 -0700

    Add render-markdown

--- init.lua
+++ init.lua
@@ -169,6 +169,14 @@ require('lazy').setup({
       fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
+  },
+  -- render-markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   }
 })
 
